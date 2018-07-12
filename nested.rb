a36d46d2440f7f1b4182d45398eac3fda5bed4d5
Hugo Escandon <hescandon6@gmail.com>
2018-07-12 16:36:09 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -39,11 +39,7 @@ def alan_kay_is_known_for
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        },
-				:yukihiro_matsumoto => {
-				   :known_for => "Ruby",
-				    :languages => ["LISP", "C"]
-				}
+        }
      }
 		 programmer_hash[:alan_kay][:known_for] 
 
